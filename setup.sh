72fe495c590f73beb4f260b56974af123a645115
Alexander Clouter <alex@coremem.com>
2022-05-10 14:41:00 +0100

    disable caching

--- setup.sh
+++ setup.sh
@@ -21,25 +21,18 @@ server:
     access-control: ::1 allow_snoop
 EOF
 
-# https://unbound.docs.nlnetlabs.nl/en/latest/topics/serve-stale.html
-cat <<'EOF' > "/opt/$VENDOR/$PROJECT/services/unbound/unbound.conf.d/serve-stale.conf"
-server:
-    serve-expired: yes
-    serve-expired-ttl: 86400            # one day, in seconds
-    serve-expired-client-timeout: 1800  # 1.8 seconds, in milliseconds
-EOF
-
 # must use forward-zone as stub-zone turns off RD and upstream returns REFUSED
 # https://unbound.docs.nlnetlabs.nl/en/latest/manpages/unbound.conf.html#term-local-zone-zone-type
 # https://unbound.docs.nlnetlabs.nl/en/latest/manpages/unbound.conf.html#forward-zone-options
 # https://unbound.docs.nlnetlabs.nl/en/latest/manpages/unbound.conf.html#stub-zone-options
 cat <<'EOF' > "/opt/$VENDOR/$PROJECT/services/unbound/unbound.conf.d/zone.conf"
 server:
-  local-zone: "." always_refuse
-  local-zone: "soas.ac.uk." always_transparent
+    local-zone: "." always_refuse
+    local-zone: "soas.ac.uk." always_transparent
 
 forward-zone:
     name: soas.ac.uk
+    forward-no-cache: yes
     # https://docs.microsoft.com/en-us/azure/virtual-network/what-is-ip-address-168-63-129-16
     forward-addr: 168.63.129.16
 EOF
