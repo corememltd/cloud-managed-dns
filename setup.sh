431abed2665fb1a2eb8c4a9be008a14ec8ca508a
Alexander Clouter <alex@coremem.com>
2022-05-12 14:55:05 +0100

    move unbound files out of setup.sh into services directory

--- setup.sh
+++ setup.sh
@@ -26,36 +26,6 @@ fi
 
 shred -u /tmp/$VENDOR-$PROJECT.git
 
-mkdir -p "/opt/$VENDOR/$PROJECT/services/unbound/unbound.conf.d"
-
-cat <<'EOF' > "/opt/$VENDOR/$PROJECT/services/unbound/unbound.conf.d/listen.conf"
-server:
-    interface: eth0
-    interface: 127.0.0.1
-    access-control: 0.0.0.0/0 allow
-    access-control: ::/0 allow
-    access-control: 127.0.0.0/8 allow_snoop
-    access-control: ::1 allow_snoop
-EOF
-
-# https://unbound.docs.nlnetlabs.nl/en/latest/manpages/unbound.conf.html#term-domain-insecure-domain-name
-# https://unbound.docs.nlnetlabs.nl/en/latest/manpages/unbound.conf.html#term-local-zone-zone-type
-# https://unbound.docs.nlnetlabs.nl/en/latest/manpages/unbound.conf.html#forward-zone-options
-cat <<'EOF' > "/opt/$VENDOR/$PROJECT/services/unbound/unbound.conf.d/zone.conf"
-server:
-    domain-insecure: "soas.ac.uk."
-
-    local-zone: "." always_refuse
-    local-zone: "soas.ac.uk." always_transparent
-
-# must use forward-zone as stub-zone turns off RD and upstream returns REFUSED
-forward-zone:
-    name: soas.ac.uk
-    forward-no-cache: yes
-    # https://docs.microsoft.com/en-us/azure/virtual-network/what-is-ip-address-168-63-129-16
-    forward-addr: 168.63.129.16
-EOF
-
 find "/opt/$VENDOR/$PROJECT/services/unbound/unbound.conf.d" -type f -print0 | xargs -0 -r -t ln -f -t /etc/unbound/unbound.conf.d
 
 # https://github.com/NLnetLabs/unbound/issues/574
