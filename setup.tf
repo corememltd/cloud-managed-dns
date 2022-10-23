405e9d4536d5709b9be35773f86f6ec4bfbccb5f
Alexander Clouter <alex@coremem.com>
2022-10-23 15:36:24 +0100

    foreach

--- setup.tf
+++ setup.tf
@@ -295,7 +295,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "main" {
   for_each              = toset(var.domains)
   name                  = "vnl_${each.key}"
   resource_group_name   = azurerm_resource_group.main.name
-  private_dns_zone_name = azurerm_private_dns_zone.main[*].name
+  private_dns_zone_name = azurerm_private_dns_zone.main[each.key].name
   virtual_network_id    = azurerm_virtual_network.main.id
 }
 
